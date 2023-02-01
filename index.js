6015b0259fd429d46ac02c0f5b444a8e4b0d79f6
Brett Polacek <brettpolacek@gmail.com>
2023-01-31 20:23:43 -0500

    add index.js

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // code your solution here
+/*function saturdayFun(activity = "roller-skate") {
+    console.log(`This Saturday, I want to ${activity}!`);
+}*/
+function saturdayFun(activity = 'roller-skate'){
+    return `This Saturday, I want to ${activity}!`;
+};
+
+function mondayWork(activity = 'go to the office') {
+    return `This Monday, I will ${activity}.`;
+};
+
+
+function wrapAdjective(flair='*'){
+   const innerFunction = function(adjective ='special') {
+        return `You are ${flair}${adjective}${flair}!`;
+    };
+    return innerFunction;
+};
+
+/*function outer(greeting, msg = "It's a fine day to learn") {
+    // 2
+    const innerFunction = function (name, lang = "Python") {
+      // 3
+      return `${greeting}, ${name}! ${msg} ${lang}`; // 4
+    };
+    return innerFunction("student", "JavaScript"); // 5
+  }*/
